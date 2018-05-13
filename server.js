36d05ace4516e3e9752a2e4a38d7ed42ee0a5124
Katrin Sigurdardottir <katrinduasig@gmail.com>
2018-05-13 13:16:26 +0200

    database,logIn, signUp and more

--- server.js
+++ server.js
@@ -1,34 +1,77 @@
+//SPURJA Á ÞETTA AÐ VERA VAR EÐA CONST!?
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
 var formidable = require('express-formidable');
 var bodyParser = require('body-parser');
-// var fs = require('fs');
 global.gFs = require('fs');
 app.use(express.static(__dirname + '/public'));
 
 
-app.use(formidable());
+// app.use(formidable());
+app.use(bodyParser());
 
 const sHeaderHTML = gFs.readFileSync( __dirname + '/html/header.html', 'utf8');
 const sFooterHTML = gFs.readFileSync( __dirname + '/html/footer.html', 'utf8');
 const chatFile = require(__dirname + '/chat.js');
+const UserFile = require(__dirname + '/users.js');
 chatFile.getChat();
 
+/* *** *** ATH.. ANNAÐHVORT Á ÞETTA AÐ VERA HERNA EF VIÐ NOTUM DATAB. ANNARSTAÐAR EN Í users.js ANNARS TAKA ÚT // Taka þá út í users *** *** */
+// //OUR DATABASE
+// global.db = mysql.createConnection({
+//     host: "localhost",
+//     user: "admin",
+//     password: "password",
+//     database: "dbynad",
+//     port: 8889
+// });
 
+// //CONNET TO OUR DATABASE
+// db.connect(err => {
+//     if(err){console.log(err), process.exit()}
+//     console.log('connected');
+// });
+
+
+
+/* *** *** Home *** *** */
 app.get('/', (req, res) => {
     var sHomeHTML = gFs.readFileSync( __dirname + '/html/home.html', 'utf8');
-    
     res.send(sHeaderHTML + sHomeHTML + sFooterHTML);
 });
 
-
+/* *** *** Chat *** *** */
 app.get('/chat', (req, res) => {
     var sChatHTML = gFs.readFileSync( __dirname + '/html/chat.html', 'utf8');
     res.send(sHeaderHTML + sChatHTML + sFooterHTML);
 });
 
-//LISTENING TO PORT
+/* *** *** Pieces *** *** */
+app.get('/pieces', (req, res) => {
+    var sPiecesHTML = gFs.readFileSync( __dirname + '/html/pieces.html', 'utf8');
+    res.send(sHeaderHTML + sPiecesHTML + sFooterHTML);
+});
+
+/* *** *** LogIn Page *** *** */
+app.get('/log-in', (req, res) => {
+    var sLogInHTML = gFs.readFileSync( __dirname + '/html/log-in.html', 'utf8');
+    res.send(sHeaderHTML + sLogInHTML + sFooterHTML);
+});
+
+/* *** *** SignUp Page *** *** */
+app.get('/sign-up', (req, res) => {
+    var sHomeHTML = gFs.readFileSync( __dirname + '/html/home.html', 'utf8');
+    var sSignUpHTML = gFs.readFileSync( __dirname + '/html/sign-up.html', 'utf8');
+    res.send(sHeaderHTML + sHomeHTML + sSignUpHTML + sFooterHTML);
+});
+/* *** *** Add User *** *** */
+app.post('/submit-sign-up', (req, res) => {
+    UserFile.saveUser(req, res);
+});
+
+
+/* *** *** LISTENING TO PORT *** *** */
 var port = 1983;
 app.listen(port, err => {
     if(err) {
--- users.js
+++ users.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
-//OUR DATABASE
-var db = mysql.createConnection({
+/* *** *** OUR DATABASE *** *** */
+global.db = mysql.createConnection({
     host: "localhost",
     user: "admin",
     password: "password",
@@ -9,51 +9,76 @@ var db = mysql.createConnection({
     port: 8889
 });
 
-//CONNET TO OUR DATABASE
+/* *** *** CONNET TO OUR DATABASE *** *** */
 db.connect(err => {
     if(err){console.log(err), process.exit()}
     console.log('connected');
 });
 
+jUser = {};
 
-//SELECT ALL USERS FROM DATABASE
-var stmt = 'SELECT * FROM users';
-db.query(stmt, (err, ajData)=>{
-    console.log(ajData)
-});
+/* *** *** SELECT ALL USERS *** *** */
+// getUsers = function(req,res) {
+//     var stmt = 'SELECT * FROM users';
 
-//INSERT USER
-var jUserData = {
-    'firstname': 'Anne Mai',
-    'lastname': 'Særker-Sørensen',
-    'profession': 'Web Developer',
-    'description': 'Im a web Designer',
-    'email': 'annemai@gmail.com',
-    'password': '123',
-    'profile_image': 'https://www.facebook.com/photo.php?fbid=10156189333629698&set=a.10150098855654698.298997.616564697&type=3&theater',
-    'phone_number': '50505050',
-    'instagram_url': 'https://www.instagram.com/annesaerker/',
-    'facebook_url': 'https://www.facebook.com/annesaerker',
-    'roles_idroles': '2',
-    'location_idlocation': '1',
+//     db.query(stmt, (err, ajData)=>{
+//         if(err){
+//             return res.send('We had some problem getting all the users');
+//         }
+//         console.log(ajData)
+//     });
+// } 
+
+/* *** *** INSERT USER *** *** */
+jUser.saveUser = function(req, res) {
+	var jUserData = {
+        'firstname': req.body.user.firstname,
+        'lastname': req.body.user.lastname,
+        'profession': req.body.user.profession,
+        'description': req.body.user.description,
+        'email': req.body.user.email,
+        'password': req.body.user.password,
+        'phone_number': req.body.user.phone_number,
+        'instagram_url': req.body.user.instagram_url,
+        'facebook_url': req.body.user.facebook_url,
+        'twitter_url': req.body.user.twitter_url,
+        'profile_image': 'https://www.facebook.com/photo.php?fbid=10156189333629698&set=a.10150098855654698.298997.616564697&type=3&theater',
+        'roles_idroles': '2',
+        'location_idlocation': '1',
+    }
+    // console.log(req.body.user.firstname);    
+    //console.log(jUserData);
+
+    var stmt = 'INSERT INTO users SET ?';
+    db.query(stmt, jUserData, (err, jData) => {
+        if(err) {
+            return res.send('error');
+        }
+        if(jData.affectedRows == 1){
+            console.log('great, JSON user inserted');
+            res.json({success: 'ok'});
+        }
+    });
 }
-var stmt = 'INSERT INTO users SET ?';
-// db.query(stmt, jUserData, (err, jData) => {
-//     if(jData.affectedRows == 1){
-//         console.log('great, JSON user inserted');
-//     }
+
+module.exports = jUser;
+
+// //SELECT ALL USERS FROM DATABASE
+// var stmt = 'SELECT * FROM users';
+// db.query(stmt, (err, ajData)=>{
+//     console.log(ajData)
 // });
 
-//DELETE
-var sName = [7, 'A'];
-var stmt = 'DELETE FROM `users` WHERE iId = ? AND sName = ?';
-// db.query(stmt, sName, (err, jData) => {
-//     console.log("jData ", jData);
-// })
+// //DELETE
+// var sName = [7, 'A'];
+// var stmt = 'DELETE FROM `users` WHERE iId = ? AND sName = ?';
+// // db.query(stmt, sName, (err, jData) => {
+// //     console.log("jData ", jData);
+// // })
 
-//UPDATE
-var sName = ['KATRIN', 9];
-var stmt = 'UPDATE `users` SET sName = ? WHERE iId = ?';
-// db.query(stmt, sName, (err, jData) => {
-//     console.log("jData ", jData);
-// })
+// //UPDATE
+// var sName = ['KATRIN', 9];
+// var stmt = 'UPDATE `users` SET sName = ? WHERE iId = ?';
+// // db.query(stmt, sName, (err, jData) => {
+// //     console.log("jData ", jData);
+// // })
